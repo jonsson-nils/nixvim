9a7cb821051416f006cdbdcc60018f1f2eb5cc3d
Nils Jonsson <nils.jonsson@jonsson-analytics.com>
2026-02-25 14:08:04 +0100

    Fixed macro expansion with rust analyzer.

--- config/default.nix
+++ config/default.nix
@@ -171,6 +171,7 @@
           enable = true;
           installCargo = false; # Let Nix manage cargo/rustc
           installRustc = false;
+          settings.procMacro.enable = true;
         };
 
         # Nix: Uses nixd (highly recommended for flakes)
