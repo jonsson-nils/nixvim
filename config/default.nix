486ae8289b038ec993cfa92e35a291bc6b5675e3
Nils Jonsson <nils.jonsson@jonsson-analytics.com>
2026-03-06 16:48:54 +0100

    Installed plugin for ANSI escaping in terminal logs.

--- config/default.nix
+++ config/default.nix
@@ -19,4 +19,24 @@
   diagnostic = import ./diagnostic.nix { inherit pkgs; };
   keymaps = import ./keymaps.nix { inherit pkgs; };
   plugins = import ./plugins { inherit pkgs; };
+
+  extraPlugins = [
+    (pkgs.vimUtils.buildVimPlugin {
+      name = "ansi-nvim";
+      src = pkgs.fetchFromGitHub {
+        owner = "0xferrous";
+        repo = "ansi.nvim";
+        rev = "main"; # Use a commit hash for reproducibility
+        sha256 = "sha256-Kv7xDWqKbF0jVtGZDRYDoN7UVl22E/If3vRHMmQAmNA="; # Replace with actual hash
+      };
+    })
+  ];
+
+  # Initialize the plugin in Lua
+  extraConfigLua = ''
+    require("ansi").setup({
+      auto_enable = true,     -- Automatically color logs if desired
+      filetypes = { "log" },  -- Target the filetype we set in the tmux binding
+    })
+  '';
 }
